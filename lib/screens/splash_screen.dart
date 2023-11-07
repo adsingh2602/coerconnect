ac22a26944cd5b2be7048f3d9dc9d0945efc80ae
Amardeep Singh <adsingh26@gmail.com>
2023-11-07 15:30:09 +0530

    modify

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:coreconnect/screens/login.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {  
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+
+
+
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 1), (){
+      Navigator.pushReplacement(context, 
+      MaterialPageRoute(
+        builder: (context) => LoginPage())
+      );
+
+    });
+
+  
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: Center(child: Text('COERConnect', style: TextStyle(
+          fontSize: 34,
+          fontWeight: FontWeight.w700,
+          color: Colors.white
+        ),)),
+      ),
+
+    );
+
+  
+  }
+}
